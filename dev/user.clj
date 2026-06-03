38402f792f51eb2ad9f91cee46c4daedb6d1f707
duke <duke@github.com>
2026-06-03 16:03:46 -0400

    repl-mcp

--- dev/user.clj
+++ dev/user.clj
@@ -3,7 +3,6 @@
 ((requiring-resolve 'clojure+.error/install!))
 ((requiring-resolve 'clojure+.hashp/install!))
 ((requiring-resolve 'clojure+.print/install!))
-((requiring-resolve 'clojure+.test/install!))
 
 (require '[clj-reload.core :as reload])
 (reload/init {:dirs ["src/main/clj" "src/test/clj"]})
